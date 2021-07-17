15cbea05cd17808810d2e402f7b00c07d3de1efa
SankethGB <sankethgb2000@gmail.com>
2021-07-17 23:03:27 +0530

    commit2

--- backend/routers/AuthRoute.js
+++ backend/routers/AuthRoute.js
@@ -1,18 +1,17 @@
 import express from "express";
-import User from "../models/userModel.js";
+import User from "../models/Uermodels.js";
 import asyncHandler from "express-async-handler";
 import passport from "passport";
 import AuthController from "../controllers/AuthControllers/Index.js";
 
-const router=express.Router()
-
+const router = express.Router();
 
 router.get("/", (req, res) =>
-res.send("Welcome to my Google Oauth express server")
+  res.send("Welcome to my Google Oauth express server")
 );
 
 router.post(
-"/oauth/google",
-passport.authenticate("google-token", { session: false }),
-AuthController.googleLogin
+  "/oauth/google",
+  passport.authenticate("google-token", { session: false }),
+  AuthController.googleLogin
 );
--- frontend/src/Gauth.js
+++ frontend/src/Gauth.js
@@ -1,44 +1,82 @@
-import axios from 'axios';
-import Cookie from 'js-cookie';
-import { GoogleLogin } from 'react-google-login';
-export const axiosApiCall = (url, method, body = {}) => axios({
+import axios from "axios";
+import Cookie from "js-cookie";
+import { GoogleLogin } from "react-google-login";
+
+const { google } = require("googleapis");
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
+  "http://localhost:3000"
+);
+
+export const axiosApiCall = (url, method, body = {}) =>
+  axios({
     method,
     url: `${process.env.REACT_APP_API_BASE_URL}${url}`,
     data: body,
   });
 export default function Gauth() {
   const onGoogleSuccess = (response) => {
-    const access_token = response.accessToken;
-    axiosApiCall(
-      '/auth/google',
-      'post',
-      { access_token }
-    ).then((res) => {
-      const { user, token } = res.data;
-      Cookie.set('token', token);
-    }).catch((err) => {
-      throw new Error(err);
-    })
-  }
-  const onGoogleFailure = () => {console.log("boomer")}
+    const code = response.code;
+    const profile = response.googleId;
+
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.log("access   token", err);
+      oAuth2Client.on("tokens", (tokens) => {
+        if (tokens.refresh_token) {
+          axiosApiCall("/auth/google", "post", {
+            refreshToken: tokens.refresh_token,
+            profile,
+          })
+            .then((res) => {
+              const { user, token } = res.data;
+              Cookie.set("token", token);
+            })
+            .catch((err) => {
+              throw new Error(err);
+            });
+        } else {
+          axiosApiCall("/auth/google", "post", {
+            accessToken: tokens.access_token,
+            profile,
+          })
+            .then((res) => {
+              const { user, token } = res.data;
+              Cookie.set("token", token);
+            })
+            .catch((err) => {
+              throw new Error(err);
+            });
+        }
+      });
+    });
+  };
+  const onGoogleFailure = () => {
+    console.log("boomer");
+  };
   return (
-    <div style={{
-      width: '100%',
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#151a30',
-      color: 'white'
-    }}>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#151a30",
+        color: "white",
+      }}
+    >
       <h1>Google Oauth Sign In</h1>
-      <GoogleLogin 
+      <GoogleLogin
         clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
         buttonText="Sign in with Google"
         onSuccess={onGoogleSuccess}
         onFailure={onGoogleFailure}
-        className="google-login-button" />
+        accessType="offile"
+        scope="https://www.googleapis.com/auth/gmail.send"
+        className="google-login-button"
+      />
     </div>
   );
 }
